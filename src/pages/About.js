cd441b92dfc0df8b7add3a148d63ef56cf18c9bc
Mucahit Cigci <cmucayit@gmail.com>
2022-12-25 23:44:32 +0300

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,22 @@
-import logo512 from "../images/logo512.png";
-
+import React, { useState } from "react";
+import PDF from "../images/cv.pdf";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 function About() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
   return (
     <div>
       <h1>About Us </h1>
-      <img src={logo512} alt="" />
+      <Document file={PDF} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   );
 }
