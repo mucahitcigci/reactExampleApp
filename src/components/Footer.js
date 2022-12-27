4728415aadff8e94e6880d9e2ab2dd07a1033e58
Mucahit Cigci <cmucayit@gmail.com>
2022-12-27 22:25:38 +0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,75 @@
 import React from "react";
 import "./footer.css";
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 const Footer = () => {
   return (
     <div>
       <div className="footer">
-        <ul className="footerUl">
-          <li>
-            <a href={"/"}> Home</a>
-          </li>
-          <li>
-            <a href={"/about"}> About Us </a>
-          </li>
-          <li>
-            <a href={"/contact"}> Contact </a>
-          </li>
-          <li>
-            <a href={"/profiles"}> Profiles </a>
-          </li>
-
-          <li>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-              dicta explicabo magnam? Veritatis corporis vero quia adipisci
-              corrupti asperiores fuga nostrum doloribus veniam at. Totam
-              suscipit ut sapiente tempore quidem.
-            </p>{" "}
-          </li>
-        </ul>
-        <div> </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <ul className="footerUl">
+                <li>
+                  {" "}
+                  <p>
+                    <strong>Description</strong>
+                  </p>
+                  <p>
+                    {" "}
+                    My career planning, frontend in the Web field I designed it
+                    as a developer. The first rule of a successful life is the
+                    right time. is to manage. With this motto my personal skills
+                    in the developing world I continue to work to increase it.
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul className="footerUl">
+                <li>
+                  <p>
+                    <strong>Navigation</strong>
+                  </p>
+                  <a href={"/"}> Home</a>
+                </li>
+                <li>
+                  <a href={"/about"}>About Us </a>
+                </li>
+                <li>
+                  <a href={"/contact"}> Contact </a>
+                </li>
+                <li>
+                  <a href={"/profiles"}> Profiles </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul className="footerUl">
+                <li>
+                  <p>
+                    <strong>Social Media</strong>
+                  </p>
+                  <div>
+                    <a className="iconStyle">
+                      <AiOutlineInstagram />{" "}
+                    </a>{" "}
+                    <a className="iconStyle">
+                      {" "}
+                      <AiOutlineTwitter />{" "}
+                    </a>
+                    <a className="iconStyle">
+                      <AiOutlineLinkedin />
+                    </a>{" "}
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="copyright">
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,56 @@
 import React from "react";
-import PDFViewer from "pdf-viewer-reactjs";
-import PDF from "../images/cv.pdf";
-
+import "./About.css";
+import pic from "../images/aboutPic.jpg";
+import { AiOutlinePhone } from "react-icons/ai";
+import { CgWebsite } from "react-icons/cg";
+import { BiMailSend } from "react-icons/bi";
 function About() {
   return (
     <div>
       <h1>About Us </h1>
-      <PDFViewer
-        document={{
-          base64: `${PDF}`,
-        }}
-      />
+      <div className="container">
+        <div className="row">
+          <div className="col leftSide">
+            <img src={pic} alt="" />
+            <h2> Mücahİt ÇIğcİ</h2>
+            <p>Front-End Web Developer</p>
+            <h2>About Me</h2>
+            <p>
+              <span>
+                My career planning, frontend in the Web field I designed it as a
+                developer. The first rule of a successful life is the right
+                time. is to manage. With this motto my personal skills in the
+                developing world I continue to work to increase it. In order to
+                educate myself in the web field, I have many in these trainings
+                with my research that I have gained competence and continue to
+                do so I should point out. Algorithm to success designs and in
+                line with this design I'm moving forward. Innovative, skills and
+                processes continuous improvement oriented, enthusiastic,
+                durable, compatible, collaborative, prone to teamwork and i am
+                smiling.
+              </span>
+            </p>
+            <h2>Contact</h2>
+            <ul>
+              <li>
+                <a href="https://mucahitcigci.com" target="_blank">
+                  {" "}
+                  <CgWebsite className="icn" /> https://mucahitcigci
+                </a>
+              </li>
+              <li>
+                <AiOutlinePhone className="icn" />{" "}
+                <a href="tel:+905449457677">05449457677</a>
+              </li>
+              <li>
+                <BiMailSend className="icn" />{" "}
+                <a href="mailto:cmucayit@gmail.com">cmucayit@gmail.com</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col rightSide">s</div>
+        </div>
+      </div>
     </div>
   );
 }
